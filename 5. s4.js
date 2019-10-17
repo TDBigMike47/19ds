142b549ceb94a4663be971b69d8fb562febe47a1
TDBigMike47 <43075910+TDBigMike47@users.noreply.github.com>
2019-10-17 18:22:28 -0500

    Delete 5. s4.js

--- 5. s4.js
+++
@@ -1,9 +0,0 @@
-var K=5;
-var sum=0, res=0;
-for(i=1; i<11; i++){
-    sum+=Math.pow(i,K);
-}
-console.log("Resultado 1 ="+ sum);
-res=(Math.pow(10,6))/(K+1);
-console.log("Resultado 2 =" +res);
-console.log("Comprobación = "+(res-sum));

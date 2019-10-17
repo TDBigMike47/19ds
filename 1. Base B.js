de3d9394a9ce6a59643aa9f9d7a8faf0ae57b429
TDBigMike47 <43075910+TDBigMike47@users.noreply.github.com>
2019-10-17 18:19:03 -0500

    Delete 1. Base B.js

--- 1. Base B.js
+++
@@ -1,5 +0,0 @@
-var valor;
-valor = prompt("Introduce el valor de la base que se utilizara para el log")
-for(i=1; i<11; i++){
-    console.log("El logaritmo base "+valor+ " de "+i+" es = "+ (Math.log(i)/Math.log(valor)));
-}           

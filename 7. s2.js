aa4cdc99a783f0b34cb784f856486f4537a3ba45
TDBigMike47 <43075910+TDBigMike47@users.noreply.github.com>
2019-10-17 18:22:46 -0500

    Delete 7. s2.js

--- 7. s2.js
+++
@@ -1,8 +0,0 @@
-var sum=0, res=0;
-for(i=1; i<11; i++){
-    sum+=i;
-}
-console.log("Resultado 1 (Sumatoria)="+ sum);
-res=(10*(10+1)/2);
-console.log("Resultado 2 (Forma cerrada)=" +res);
-console.log("Comprobación = "+(res-sum));
